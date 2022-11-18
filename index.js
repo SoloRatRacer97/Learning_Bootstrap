e9763816c4d50209cd51e968198b6db7bd950cc4
SoloRatRacer97 <todd.j.anderson.1997@gmail.com>
2022-11-18 14:04:07 -0800

    Original commit. Originally created June 2022

---
+++ index.js
@@ -0,0 +1,37 @@
+
+
+
+
+const express = require('express');
+const app = express();
+const path = require(`path`)
+
+app.use(express.static(path.join(__dirname, "/public")));
+
+app.set(`view engine`, `ejs`)
+app.set(`views`, path.join(__dirname, '/views'))
+
+app.get(`/`, (req, res) => {
+        res.render(`home`)
+})
+
+app.get('/cats', (req, res) => {
+        const cats = [
+                'Blue', 'Rocket', 'John', 'Stank'
+        ]
+        res.render('cats', {  cats })
+})
+
+app.get(`/r/:subreddit`, (req, res) => {
+        const { subreddit } = req.params;
+        res.render(`subreddit`, {subreddit})
+})
+
+app.get(`/rand`, (req,res) => {
+        const num = Math.floor(Math.random() * 10) + 1;
+        res.render(`random`, { rand: num})
+})
+
+app.listen(3000, () => {
+        console.log(`Listening on Port 3000`);
+})
